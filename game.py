e9168617479a6ee7c62d12af67b705248a87c508
jfunky11 <82351460+jfunky11@users.noreply.github.com>
2022-01-15 18:03:29 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+#plays game
 def main():
     player = nextPlayer("")
     board = createBoard()
@@ -8,12 +9,14 @@ def main():
     displayBoard(board)
     print("Good game. Thanks for playing!") 
 
+#gives range of squares and creates the board
 def createBoard():
     board = []
     for square in range(9):
         board.append(square + 1)
     return board
 
+#displays how the board will look
 def displayBoard(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
@@ -22,13 +25,15 @@ def displayBoard(board):
     print('-+-+-')
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
-    
+
+#makes the game end in a draw if all squares are used
 def draw(board):
     for square in range(9):
         if board[square] != "x" and board[square] != "o":
             return False
     return True 
-    
+
+#all combinations to win
 def winner(board):
     return (board[0] == board[1] == board[2] or
             board[3] == board[4] == board[5] or
@@ -39,10 +44,12 @@ def winner(board):
             board[1] == board[4] == board[7] or
             board[2] == board[4] == board[6])
 
+#start next player's move
 def makeMove(player, board):
     square = int(input(f"{player}'s turn to choose a square (1-9): "))
     board[square - 1] = player
 
+#determines whether the current player places an x or an o
 def nextPlayer(current):
     if current == "" or current == "o":
         return "x"
