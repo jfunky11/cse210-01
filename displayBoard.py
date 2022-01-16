f3b3f7afff3f32cb9f964c7929246b51593ed13e
jfunky11 <82351460+jfunky11@users.noreply.github.com>
2022-01-15 18:13:44 -0800

    Create displayBoard.py

---
+++ displayBoard.py
@@ -0,0 +1,16 @@
+def main():
+  displayBoard()
+  pass
+
+#displays how the board will look
+def displayBoard(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+  
+if __name__ == '__main__':
+    main()
